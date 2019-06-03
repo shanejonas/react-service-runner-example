b2a108a08e596f0e41e352787b7805fafde91f99
shanejonas <jonas.shane@gmail.com>
2019-06-03 11:00:05 -0700

    fix: use service runner react hooks npm package

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,58 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
-import useService from "./useService";
-import useServiceRunnerInfo from "./useServiceRunnerInfo";
 import { TaskService } from "@etclabscore/jade-service-runner-client";
+import { useServiceRunner } from "@etclabscore/jade-service-runner-react-hooks";
 
 const ethjsUnit = require("ethjs-unit"); //tslint:disable-line
 
-const App: React.FC = () => {
-  const [installed, service] = useService("multi-geth", "1.9.0", "kotti");
-  const [runningServices] = useServiceRunnerInfo();
+interface IServiceProps {
+  item: TaskService;
+  title?: string;
+  onClick?: () => any;
+}
+const Service: React.FC<IServiceProps> = (props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+    <div>
+      <span>
+        {props.item.name}
+      </span>
+      |
+      <span>
+        {props.item.env}
+      </span>
+      |
+      <span style={{ backgroundColor: "#aeaeae" }} onClick={props.onClick}>
+        {props.title}
+      </span>
+    </div>
+  );
+};
+
+const App: React.FC = () => {
+  const [supportedServices, runningServices, installedServices, installService, startService] = useServiceRunner();
+  return (
+    <>
+      <div>
+        <h5>Supported Services:</h5>
+        {supportedServices && supportedServices.map((s: any) => {
+          return (
+            <Service item={s} title={"INSTALL"} onClick={() => installService(s.name, s.version)}></Service>
+          );
+        })}
+        <h5>Installed Services:</h5>
+        {installedServices && installedServices.map((s: any) => {
+          return (
+            <Service item={s} title={"RUN"} onClick={() => startService(s.name, s.version, "kotti")}></Service>
+          );
+        })}
         <h5>Running Services:</h5>
         {runningServices && runningServices.map((s: TaskService) => {
           return (
-            <div>{s.name}</div>
-          )
+            <Service item={s}></Service>
+          );
         })}
-        {!installed && <div>Service Installing</div>}
-        {installed && <div>Service Installed</div>}
-        {!service && <div>Service not running.</div>}
-        {service && <div>Running Service {service.name}, env: {service.env} on localhost:{service.rpcPort}</div>}
-      </header>
-    </div>
+      </div>
+    </>
   );
 };
 
